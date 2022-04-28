9171d0e11261418857f3c0a8b4cf18dddd42ab62
Alphacharge <rbetz@1-B-16.42heilbronn.de>
2022-04-28 11:37:51 +0200

    norm io final version

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 11:33:22 by rbetz             #+#    #+#             */
-/*   Updated: 2022/04/27 17:20:38 by rbetz            ###   ########.fr       */
+/*   Updated: 2022/04/28 10:46:12 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 int		ft_printf(const char *format, ...);
 int		ft_loop_format(const char *format, int chars, va_list args);
-int 	ft_flaghandling(char c, va_list args);
+int		ft_flaghandling(char c, va_list args);
 int		ft_hex(unsigned long number, char c);
 int		ft_counthex(unsigned long c);
 void	*ft_calloc(size_t count, size_t size);
